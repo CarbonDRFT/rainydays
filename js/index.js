a5eadbc0a81b1fd2dbc691df9bd48ba6df69031c
CarbonDRFT <67243227+CarbonDRFT@users.noreply.github.com>
2020-11-02 23:10:41 +0100

    Last changes to the assignment

--- js/index.js
+++ js/index.js
@@ -1,6 +1,6 @@
 let popup = document.getElementById("Contact-popup");
 let contactFooter = document.getElementById("Contact-popup-footer");
-let popupup = document.getElementById("Policy");
+let purchaseComplete = document.getElementById("purchase__completed--btn");
 
 popup.addEventListener("click", () => {
   document.getElementById("contact_popup_page").style.display = "block";
@@ -10,9 +10,15 @@ contactFooter.addEventListener("click", () => {
   document.getElementById("contact_popup_page").style.display = "block";
 });
 
+purchaseComplete.addEventListener("click", () => {
+  document.getElementById("purchase__complete").style.display = "block";
+});
+
 let close = document.getElementById("close");
 
 let contactSubmit = document.getElementById("contact-submit");
+
+let closePurchase = document.getElementById("close__purchase--popup");
 
 close.addEventListener("click", () => {
   document.getElementById("contact_popup_page").style.display = "none";
@@ -20,4 +26,8 @@ close.addEventListener("click", () => {
 
 contactSubmit.addEventListener("click", () => {
   document.getElementById("contact_popup_page").style.display = "none";
+});
+
+closePurchase.addEventListener("click", () => {
+  document.getElementById("purchase__complete").style.display = "none";
 });
